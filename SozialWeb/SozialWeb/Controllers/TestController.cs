1f8f3b1bde1f2f90028c290dc501ca3c56118555
Grímur Garpsson <gogo.gogo520@gmail.com>
2015-05-06 14:26:04 +0000

    Log In Default

---
+++ SozialWeb/SozialWeb/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SozialWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index() 
+        {
+            return "This is an index page";
+        }
+
+        public string TestPage()
+        {
+            return "This is a test page";
+        }
+    }
+}
