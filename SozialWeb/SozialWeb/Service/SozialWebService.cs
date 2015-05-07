67f68e0f8fef5aa78a060c486d83a566c625f3c6
Bjarni Gudmundsson <bjarnigud13@ru.is>
2015-05-07 20:08:31 +0000

    Profilefix

---
+++ SozialWeb/SozialWeb/Service/SozialWebService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SozialWeb.Models;
+using System.Web.Mvc;
+
+namespace SozialWeb.Service
+{
+    public class SozialWebService
+    {
+
+    }
+}
