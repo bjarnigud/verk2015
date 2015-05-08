779ad6d210f82ba9a129ff68c6cd52455ce267fd
Heimir Örn Jóhannesson <heimiroj02@ru.is>
2015-05-08 15:42:19 +0000

    Deleting DAL and working on friendlist

--- SozialWeb/SozialWeb/Controllers/FriendsListController.cs
+++ SozialWeb/SozialWeb/Controllers/FriendsListController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SozialWeb.Models;
+using Microsoft.AspNet.Identity;
 
 namespace SozialWeb.Controllers
 {
@@ -12,6 +14,19 @@ namespace SozialWeb.Controllers
         public ActionResult FriendsListView()
         {
             return View();
+        }
+
+        public ActionResult FriendsListTestView()
+        {
+            
+            var userId = User.Identity.GetUserId();
+            List<ApplicationUser> friends = new List<ApplicationUser>();
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var friendsList = from n in db.Users
+                                  select n;
+            var variabel = "HELLO";
+            return View(friendsList);
         }
     }
 }
