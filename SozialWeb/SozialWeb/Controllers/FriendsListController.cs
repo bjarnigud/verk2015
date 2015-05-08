86b8531787a2f212d4fa668caf6ba6d9d14a9504
Heimir Örn Jóhannesson <heimiroj02@ru.is>
2015-05-08 21:38:02 +0000

    Added FriendListServices

--- SozialWeb/SozialWeb/Controllers/FriendsListController.cs
+++ SozialWeb/SozialWeb/Controllers/FriendsListController.cs
@@ -5,33 +5,27 @@ using System.Web;
 using System.Web.Mvc;
 using SozialWeb.Models;
 using Microsoft.AspNet.Identity;
+using SozialWeb.Service;
 
 namespace SozialWeb.Controllers
 {
     public class FriendsListController : Controller
     {
         // GET: FriendsList
+        /*
         public ActionResult FriendsListView()
         {
             return View();
         }
-
-        public ActionResult FriendsListTestView()
+        */
+        public ActionResult FriendsListView()
         {
             
             var userId = User.Identity.GetUserId();
             ApplicationDbContext db = new ApplicationDbContext();
-            /*
-            var friendsList = from user in db.Users
-                              join friend in db.FriendLists on user.Id equals friend.friend1.Id into something
-                              where userId equals user.Id
-                              select new { Cate = something };
-          
-            */
-            var friendsList = from user in db.Users
-                              select user;
 
-            var variabel = "HELLO";
+            FriendListService f = new FriendListService();
+            var friendsList = f.getFriends(userId);
             return View(friendsList);
         }
     }
---
+++ SozialWeb/SozialWeb/Service/FriendListService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SozialWeb.Models;
+
+namespace SozialWeb.Service
+{
+    public class FriendListService
+    {
+        public List<ApplicationUser> getFriends(string userId)
+        {
+            List<ApplicationUser> friendList = new List<ApplicationUser>();
+            ApplicationDbContext db = new ApplicationDbContext();
+            var allUsers = from user in db.Users
+                           select user;
+
+
+            var allFriends = from friend in db.FriendLists
+                             where userId == friend.friend1.Id
+                             select friend.friend2;
+            List<ApplicationUser> currentUserFriendsList = new List<ApplicationUser>();
+
+            foreach (ApplicationUser friend in allFriends)
+            {
+                currentUserFriendsList.Add(friend);
+            }
+
+
+            return currentUserFriendsList;
+        }
+    }
+}
