e8ef9e3379d30874ad1cf3f499429073ae827249
Bjarni Gudmundsson <bjarnigud13@ru.is>
2015-05-14 16:07:20 +0000

    Sælir Núna

---
+++ SozialWeb/SozialWeb/ViewModel/FrontaPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SozialWeb.ViewModel
+{
+    public class FrontaPageViewModel
+    { 
+
+    }
+}
