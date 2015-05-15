f0a3c375d1806c0e56bfa20e7431658d202ac3ab
Heimir Örn Jóhannesson <heimiroj02@ru.is>
2015-05-15 15:04:42 +0000

    moved files around and minor fixes

--- SozialWeb/SozialWeb/Controllers/FriendsListController.cs
+++ SozialWeb/SozialWeb/Controllers/FriendsListController.cs
@@ -45,5 +45,50 @@ namespace SozialWeb.Controllers
             return RedirectToAction(returnurl);                                 //notandi sendur aftur þaðan sem hann kom
 
         }
+
+        public ActionResult SendFriendRequest(string reciverId)
+        {
+            FriendListService f = new FriendListService();
+            var senderId = User.Identity.GetUserId();
+
+            bool test = f.alreadyFriends(reciverId, senderId);          //Athugar hvort sé verið að senda vinabeiðni á vin sinn, ef svo fer á error síðu
+            if (test == true)
+            {
+                return RedirectToAction("TestError", "Test", new { errorMessage = "You are already friend with that user silly" });
+            }
+
+            if (senderId == reciverId)                                  //Athugar hvort sé verið að senda sjálfum sér vinabeiðni og ef svo er  
+            {                                                          // fer notandi á villusíðu vegna þess að það er sorglegt að senda sjálfum sér vinabeiðni
+                return RedirectToAction("TestError", "Test", new { errorMessage = "Can't send a friend request to yourself" });
+            }
+            bool already;
+            already = f.alreadyFriendRequest(senderId, reciverId);
+            if (f.alreadyFriendRequest(senderId, reciverId))
+            {
+                return RedirectToAction("TestError", "Test", new { errorMessage = "Friend request already sent, be patient" });
+            }
+
+            f.sendFriendRequest(senderId, reciverId);
+
+            return View();
+        }
+
+        public ActionResult SeeFriendRequests()
+        {
+            FriendListService fr = new FriendListService();
+            List<FriendRequest> userRequests = new List<FriendRequest>();
+            var userId = User.Identity.GetUserId();
+            userRequests = fr.getAllFriendRequestsReciver(userId);
+            return View(userRequests);
+        }
+
+        public ActionResult AcceptFriendRequest(int id)
+        {
+            FriendListService fr = new FriendListService();
+            FriendRequest f = fr.getFriendListById(id);
+            fr.addFriend(f.requestReciver.Id, f.requestSender.Id);
+            fr.deleteFriendRequest(id);
+            return View();
+        }
     }
 }
